c60eb62efcc83e11efafa2ac1f01d0a536602e61
mayar19 <54380002+mayar19@users.noreply.github.com>
2019-11-22 17:45:45 +0200

    Update 2nd-Function.js

--- 2nd-Function.js
+++ 2nd-Function.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const name = './file';
 function Reader(){
-fs.appendFile(path, 'Hello content!', function (err) {
+fs.appendFile(name, 'Hello content!', function (err) {
     if (err) throw err;
     console.log('Saved!');
   });};
